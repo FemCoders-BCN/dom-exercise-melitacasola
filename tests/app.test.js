a03b9cf2f8ec1fbe245888321a8ed5a801eeef66
melitacasola <melissacasola@gmail.com>
2023-11-20 08:40:36 +0100

    subiendo el ejercicio

---
+++ tests/app.test.js
@@ -0,0 +1,19 @@
+import {describe ,expect, it, beforeAll} from 'vitest'
+import {JSDOM} from 'jsdom'
+
+describe('APP',()=>{
+    let dom;
+    beforeAll(async () => {
+        dom = await JSDOM.fromFile("index.html", {
+          resources: "usable",
+          runScripts: "dangerously",
+        });
+      });
+      it("should render css", async () => {
+        let document = dom.window.document;
+        let link = document.querySelector("link");
+        console.log(link.href);
+        // expect(link.href).toMatch(/\/src\/css\/style.css$/);
+        // expect(link.href).toMatch;
+      });
+})
