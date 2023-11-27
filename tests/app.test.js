f9543648b20c334134b51f1cb43903c58366e26e
melitacasola <melissacasola@gmail.com>
2023-11-28 00:06:14 +0100

    no puedo configurar test

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,11 +9,19 @@ describe('APP',()=>{
           runScripts: "dangerously",
         });
       });
-      it("should render css", async () => {
+      // it("should render css", async () => {
+      //   let document = dom.window.document;
+      //   let link = document.querySelector("link");
+      //   console.log(link.href);
+      //   expect(link.href).toMatch(/\/src\/css\/style.css$/);
+      //   // expect(link.href).toMatch;
+      // });
+      it("should render CSS", async () => {
         let document = dom.window.document;
-        let link = document.querySelector("link");
+        let link = document.querySelector("link[rel='stylesheet']");
         console.log(link.href);
-        // expect(link.href).toMatch(/\/src\/css\/style.css$/);
-        // expect(link.href).toMatch;
+      
+        // Ajusta la expresión regular para reflejar la ruta de tu archivo de estilo
+        expect(link.href).toMatch(/\/src\/css\/style\.css$/);
       });
 })
